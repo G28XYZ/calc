bbef4aad972c1d682fa96cbc6aa88c4a9d660f08
Aleksandr Kucherenko <g28xyz@ya.ru>
2021-11-17 21:31:23 +0700

    add sum function

--- scripts/index.js
+++ scripts/index.js
@@ -7,22 +7,51 @@ const buttonsNumber = document.querySelectorAll(".number");
 
 const plus = document.querySelector(".operators__plus");
 
+let result = 0;
+let isReset = 1;
+let expression = {};
+
 function addToInput(evt) {
-  if (displayInput.textContent === "0") {
+  if (isReset) {
     displayInput.textContent = "";
+    isReset = 0;
   }
   displayInput.textContent += evt.target.textContent;
 }
 
 function addToOutput(evt) {
-  displayOutput.textContent +=
-    displayInput.textContent + evt.target.textContent;
+  displayOutput.textContent += expression.char
+    ? expression.char + displayInput.textContent + evt.target.textContent
+    : displayInput.textContent + evt.target.textContent;
 }
 
 for (let button of buttonsNumber) {
   button.addEventListener("click", addToInput);
 }
 
+function equal(evt) {
+  expression.func(evt);
+  displayOutput.textContent = result;
+  return;
+}
+
+function sum(evt) {
+  console.log(parseInt(displayInput.textContent));
+  result += parseInt(displayInput.textContent);
+  addToOutput(evt);
+  isReset = 1;
+  expression.func = sum;
+  expression.char = "+";
+}
+
 for (let operator of buttonsOperator) {
+  if (operator.textContent === "=") {
+    operator.addEventListener("click", equal);
+    continue;
+  }
+  if (operator.textContent === "+") {
+    operator.addEventListener("click", sum);
+    continue;
+  }
   operator.addEventListener("click", addToOutput);
 }
