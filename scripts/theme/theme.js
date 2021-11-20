32e27c7614de436495f62a8b2a8e1057a6470e3b
G28XYZ <g28xyz@ya.ru>
2021-11-20 17:31:29 +0700

    add android style

--- scripts/theme/theme.js
+++ scripts/theme/theme.js
@@ -1,21 +1,41 @@
 export default (function theme() {
-  const theme = "classic";
+  const activeTheme = document.querySelector(".tabs__view");
+  const themes = {
+    Классическая: "classic",
+    Андроид: "android",
+  };
+
+  let theme = themes[activeTheme.value];
+  let oldTheme = theme;
+
+  function changeTheme() {
+    theme = themes[activeTheme.value];
+    for (let selector of Object.keys(selectors)) {
+      let elems = document.querySelectorAll(`.${selector}`);
+      for (let elem of elems) {
+        removeClass(elem, `${selector}_${oldTheme}`);
+        addClass(elem, `${selector}_${theme}`);
+      }
+    }
+    oldTheme = theme;
+  }
 
   const selectors = {
     content: "content",
     display: "display",
     operator: "operator",
     number: "number",
+    calc: "calc",
   };
 
   function addClass(elem, selector) {
     elem.classList.add(selector);
   }
 
-  for (let selector of Object.keys(selectors)) {
-    let elems = document.querySelectorAll(`.${selector}`);
-    for (let elem of elems) {
-      addClass(elem, `${selector}_${theme}`);
-    }
+  function removeClass(elem, selector) {
+    elem.classList.remove(selector);
   }
+
+  changeTheme();
+  activeTheme.addEventListener("change", changeTheme);
 })();
